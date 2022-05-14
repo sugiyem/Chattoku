d3107105f78de3636793a11ac2b50cb19e7493e3
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-05-14 15:53:41 +0700

    1st Firebase Configuration
    Set up firebase in the project using environment variable. Can import firebase from "src/firebase/Config.js" to start using it.

---
+++ src/firebase/Config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import Constants from "expo-constants";
+
+const firebaseConfig = {
+    apiKey: Constants.manifest.extra.apiKey,
+    authDomain: Constants.manifest.extra.authDomain,
+    projectId: Constants.manifest.extra.projectId,
+    storageBucket: Constants.manifest.extra.storageBucket,
+    messagingSenderId: Constants.manifest.extra.messagingSenderId,
+    appId: Constants.manifest.extra.appId,
+};
+
+// Initialize Firebase
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
