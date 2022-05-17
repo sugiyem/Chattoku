0596fe6f438bed5d04b8e27937c21ce3c5c4cfe8
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-05-17 17:09:29 +0700

    Add tab navigation

--- App.js
+++ App.js
@@ -1,21 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
+import AnimeScreen from "./src/screens/AnimeScreen";
+import ChatScreen from "./src/screens/ChatScreen";
+import FriendScreen from "./src/screens/FriendScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer style={styles.container}>
+            <StatusBar style="light" />
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarStyle: { backgroundColor: "navy" },
+                    headerStyle: { backgroundColor: "darkslateblue" },
+                    headerTitleStyle: { color: "teal" },
+                }}
+            >
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        title: "Home",
+                        tabBarLabelPosition: "below-icon",
+                        tabBarIcon: ({ tintColor }) => (
+                            <Icon name="home" color="aquamarine" size={30} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Anime"
+                    component={AnimeScreen}
+                    options={{
+                        title: "Anime",
+                        tabBarLabelPosition: "below-icon",
+                        tabBarIcon: ({ tintColor }) => (
+                            <Icon
+                                name="ios-library"
+                                color="aquamarine"
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Chat"
+                    component={ChatScreen}
+                    options={{
+                        title: "Chat",
+                        tabBarLabelPosition: "below-icon",
+                        tabBarIcon: ({ tintColor }) => (
+                            <Icon
+                                name="ios-chatbox-ellipses"
+                                color="aquamarine"
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Friend"
+                    component={FriendScreen}
+                    options={{
+                        title: "Friends",
+                        tabBarLabelPosition: "below-icon",
+                        tabBarIcon: ({ tintColor }) => (
+                            <Icon
+                                name="ios-people"
+                                color="aquamarine"
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{
+                        title: "Profile",
+                        tabBarLabelPosition: "below-icon",
+                        tabBarIcon: ({ tintColor }) => (
+                            <Icon
+                                name="ios-person-circle"
+                                color="aquamarine"
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "aquamarine",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ src/screens/FriendScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const FriendScreen = () => {
+    return (
+        <View>
+            <Text>FriendScreen</Text>
+        </View>
+    );
+};
+
+export default FriendScreen;
+
+const styles = StyleSheet.create({});
