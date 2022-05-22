623b3873deea8af21e74f84ae45b892103489ff7
elben85 <elbert.benedict@gmail.com>
2022-05-22 22:24:29 +0800

    Add Login and Signup Skeleton

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,97 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Button,
+  TextInput,
+  Alert
+} from "react-native";
+import { firebase } from "../firebase/Config";
+import { useState } from "react";
 
-const LoginScreen = () => {
-    return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
-    );
+const initialState = {
+  email: "",
+  password: ""
+};
+
+const LoginScreen = ({ navigation }) => {
+  const [credentials, setCredentials] = useState(initialState);
+
+  function handleChange(event, name) {
+    setCredentials({
+      ...credentials,
+      [name]: event.target.value
+    });
+  }
+
+  console.log(credentials);
+
+  async function handleSubmit() {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .catch((e) => {
+        console.error(e);
+      });
+
+    const currentUser = await firebase.auth().currentUser;
+
+    console.log(currentUser);
+
+    if (currentUser === null) {
+      Alert.alert("Incorrect Email / Password");
+      await firebase.auth().signOut();
+    } else if (!currentUser.emailVerified) {
+      Alert.alert("Please Verify Your Email Before Logging In");
+      await firebase.auth().signOut();
+    } else {
+      navigation.navigate("Dashboard");
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.Container}>
+      <Text> Log In </Text>
+      <Text> email </Text>
+      <TextInput
+        placeholder="email"
+        value={credentials.email}
+        onChange={(e) => handleChange(e, "email")}
+      />
+      <Text> password </Text>
+      <TextInput
+        placeholder="password"
+        secureTextEntry={true}
+        value={credentials.password}
+        onChange={(e) => handleChange(e, "password")}
+      />
+      <Button onPress={handleSubmit} title="Log In" />
+      <Button
+        onPress={() => {
+          redirectToSignupScreen(navigation);
+        }}
+        style={styles.Login}
+        title="Don't Have an Account? Sign Up Here"
+      />
+    </SafeAreaView>
+  );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+function redirectToSignupScreen(navigation) {
+  navigation.navigate("Signup");
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    display: "flex",
+    rowGap: "15px",
+    height: "100vh",
+    backgroundColor: "cyan"
+  },
+  Login: {
+    marginTop: "20px",
+    alignSelf: "center"
+  }
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,163 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Button,
+  Text,
+  Alert
+} from "react-native";
+import { useState } from "react";
 import React from "react";
+import { firebase } from "../firebase/Config";
 
-const SignupScreen = () => {
-    return (
-        <View>
-            <Text>SignupScreen</Text>
-        </View>
-    );
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  confirmPass: ""
 };
+
+const SignupScreen = ({ navigation }) => {
+  const [credentials, setCredentials] = useState(initialState);
+
+  function handleChange(event, name) {
+    setCredentials({
+      ...credentials,
+      [name]: event.target.value
+    });
+  }
+
+  async function handleSubmit() {
+    console.log("clicked");
+    const isUsernameNotAvailable = await isUsernameTaken(credentials.username);
+
+    if (!isValidUsername(credentials.username)) {
+      Alert.alert(
+        "username must only contains alphanumeric characters and must at least be 1 character long"
+      );
+    } else if (isUsernameNotAvailable) {
+      Alert.alert("username is not available");
+    } else if (!isValidEmail(credentials.email)) {
+      Alert.alert("invalid!");
+    } else if (credentials.password !== credentials.confirmPass) {
+      Alert.alert("password doesn't match");
+    } else if (isPasswordTooShort(credentials.password)) {
+      Alert.alert("password must at least be 6 characters long");
+    } else {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(credentials.email, credentials.password)
+        .then(async (response) => {
+          response.user.sendEmailVerification();
+
+          const currentUID = firebase.auth().currentUser.uid;
+
+          await firebase.firestore().collection("users").doc(currentUID).set({
+            username: credentials.username
+          });
+
+          Alert.alert(
+            "Email sent.",
+            "Please verify your account before signing in."
+          );
+          firebase.auth().signOut();
+          console.log("finished");
+        })
+        .then(() => {
+          redirectToLoginScreen(navigation);
+        })
+        .catch((e) => console.error(e));
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.Container}>
+      <Text> Sign Up </Text>
+      <Text> username </Text>
+      <TextInput
+        placeholder="username"
+        value={credentials.username}
+        onChange={(e) => handleChange(e, "username")}
+      />
+      <Text> email </Text>
+      <TextInput
+        placeholder="email"
+        value={credentials.email}
+        onChange={(e) => handleChange(e, "email")}
+      />
+      <Text> password </Text>
+      <TextInput
+        placeholder="password"
+        secureTextEntry={true}
+        value={credentials.password}
+        onChange={(e) => handleChange(e, "password")}
+      />
+      <Text> confirm password</Text>
+      <TextInput
+        placeholder="confirm password"
+        secureTextEntry={true}
+        value={credentials.confirmPass}
+        onChange={(e) => handleChange(e, "confirmPass")}
+      />
+      <Button onPress={handleSubmit} title="Sign Up" />
+      <Button
+        onPress={() => {
+          redirectToLoginScreen(navigation);
+        }}
+        style={styles.Login}
+        title="Have an account? Login Now"
+      />
+    </SafeAreaView>
+  );
+};
+
+// A str is considered a valid email if it is in the form of
+// anystring@anystring.anystring
+function isValidEmail(str) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(str);
+}
+
+function isPasswordTooShort(password) {
+  return password.length < 6;
+}
+
+//check is a username is alphanumeric and contains at
+//least one char
+function isValidUsername(username) {
+  const regex = /^\s*([0-9a-zA-Z]+)\s*$/;
+  return regex.test(username);
+}
+
+async function isUsernameTaken(username) {
+  const listOfUsernames = [];
+  await firebase
+    .firestore()
+    .collection("users")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((documentSnapshot) => {
+        listOfUsernames.push(documentSnapshot.data().username);
+      });
+    });
+  return listOfUsernames.includes(username);
+}
+
+function redirectToLoginScreen(navigation) {
+  navigation.navigate("Login");
+}
 
 export default SignupScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Container: {
+    display: "flex",
+    rowGap: "15px",
+    height: "100vh",
+    backgroundColor: "cyan"
+  },
+  Login: {
+    marginTop: "20px",
+    alignSelf: "center"
+  }
+});
