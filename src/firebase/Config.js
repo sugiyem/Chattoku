384d3286df9be060a250b97fc4c14921f1ffe7c9
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-05-24 21:56:37 +0700

    Allow Chattoku to use firebase storage

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,21 +1,22 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 import Constants from "expo-constants";
 
 const firebaseConfig = {
-    apiKey: Constants.manifest.extra.apiKey,
-    authDomain: Constants.manifest.extra.authDomain,
-    projectId: Constants.manifest.extra.projectId,
-    storageBucket: Constants.manifest.extra.storageBucket,
-    messagingSenderId: Constants.manifest.extra.messagingSenderId,
-    appId: Constants.manifest.extra.appId,
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
 };
 
 // Initialize Firebase
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 export { firebase };
