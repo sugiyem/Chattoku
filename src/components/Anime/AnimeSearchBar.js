83e1535ffdeeb110e050b5dc1060fbc19bf35094
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-05-27 20:28:08 +0700

    Set button text to center

--- src/components/Anime/AnimeSearchBar.js
+++ src/components/Anime/AnimeSearchBar.js
@@ -1,63 +1,67 @@
 import {
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import React from "react";
 import AnimeFetch from "./AnimeFetch";
 
 const AnimeSearchBar = ({ value, onChangeText, navigation }) => {
-    const onFinishedFetch = (data) => {
-        navigation.push("AnimeResult", {
-            data: data,
-            search: value,
-        });
-    };
+  const onFinishedFetch = (data) => {
+    navigation.push("AnimeResult", {
+      data: data,
+      search: value,
+    });
+  };
 
-    return (
-        <View style={{ flexDirection: "row" }}>
-            <TextInput
-                value={value}
-                onChangeText={onChangeText}
-                placeholder="Search anime by original title"
-                style={styles.textInput}
-            />
-            <TouchableOpacity
-                onPress={() =>
-                    AnimeFetch({
-                        type: "Search",
-                        page: 1,
-                        search: value,
-                        onSuccesfulFetch: onFinishedFetch,
-                    })
-                }
-                style={styles.button}
-            >
-                <Text style={{ alignItems: "center" }}>Search</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        placeholder="Search anime by original title"
+        style={styles.textInput}
+      />
+      <TouchableOpacity
+        onPress={() =>
+          AnimeFetch({
+            type: "Search",
+            page: 1,
+            search: value,
+            onSuccesfulFetch: onFinishedFetch,
+          })
+        }
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Search</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default AnimeSearchBar;
 
 const styles = StyleSheet.create({
-    textInput: {
-        borderColor: "black",
-        borderWidth: 1,
-        margin: 5,
-        backgroundColor: "white",
-        color: "black",
-        borderRadius: 10,
-        flex: 3,
-    },
-    button: {
-        borderRadius: 10,
-        padding: 5,
-        margin: 5,
-        flex: 1,
-        backgroundColor: "darkslateblue",
-    },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 5,
+    backgroundColor: "white",
+    color: "black",
+    borderRadius: 10,
+    flex: 3,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 5,
+    margin: 5,
+    flex: 1,
+    backgroundColor: "darkslateblue",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+  },
 });
