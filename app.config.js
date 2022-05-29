e2d512d10ff945d080723e885c2efd31c1de9208
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-05-29 21:26:13 +0700

    Add more details to the app

--- app.config.js
+++ app.config.js
@@ -1,18 +1,22 @@
 import "dotenv/config";
 
 export default {
-    expo: {
-        name: "Chattoku",
-        slug: "Chattoku",
-        version: "1.0.0",
-        assetBundlePatterns: ["**/*"],
-        extra: {
-            apiKey: process.env.API_KEY,
-            authDomain: process.env.AUTH_DOMAIN,
-            projectId: process.env.PROJECT_ID,
-            storageBucket: process.env.STORAGE_BUCKET,
-            messagingSenderId: process.env.MESSAGING_SENDER_ID,
-            appId: process.env.APP_ID,
-        },
+  expo: {
+    name: "Chattoku",
+    slug: "Chattoku",
+    privacy: "public",
+    description:
+      "A cross platform mobile application specifically made for the otaku community",
+    icon: "./src/assets/logo.png",
+    version: "1.0.0",
+    assetBundlePatterns: ["**/*"],
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
     },
+  },
 };
