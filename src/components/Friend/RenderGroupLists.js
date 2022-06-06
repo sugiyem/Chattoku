7d8cc43491d6fa3823812f0a87ad776b5026d833
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-06-06 12:19:13 +0700

    Add group list and creation system
    Allow user to view it's current group and group invitation, as well as creating a group.

---
+++ src/components/Friend/RenderGroupLists.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import { Avatar, Icon, ListItem } from "react-native-elements";
+import {
+  leaveGroup,
+  acceptGroupInvitation,
+  declineGroupInvitation
+} from "../../firebase/HandleGroup";
+
+export const renderType = {
+  GROUP: 0,
+  GROUP_INVITATION: 1
+};
+
+export default RenderGroupLists = ({
+  type,
+  items,
+  navigation,
+  expandStatus,
+  changeExpand
+}) => {
+  const datas = [];
+
+  switch (type) {
+    case renderType.GROUP:
+      datas.push(
+        {
+          title: "Detail",
+          icon: "folder-open",
+          onPress: (item) =>
+            navigation.navigate("GroupInfo", { groupData: item })
+        },
+        {
+          title: "Message",
+          icon: "message",
+          onPress: (item) => navigation.replace("FriendList")
+        },
+        {
+          title: "Leave Group",
+          icon: "close",
+          onPress: (item) => {
+            Alert.alert(
+              "You will leave this group",
+              "This action is irreversible. Do you want to continue?",
+              [
+                {
+                  text: "Cancel"
+                },
+                {
+                  text: "Continue",
+                  onPress: () => leaveGroup(item.id)
+                }
+              ]
+            );
+          }
+        }
+      );
+      break;
+
+    case renderType.GROUP_INVITATION:
+      datas.push(
+        {
+          title: "Detail",
+          icon: "folder-open",
+          onPress: (item) =>
+            navigation.navigate("GroupRequestInfo", { groupData: item })
+        },
+        {
+          title: "Accept Invitation",
+          icon: "check",
+          onPress: (item) => acceptGroupInvitation(item.id)
+        },
+        {
+          title: "Decline Invitation",
+          icon: "close",
+          onPress: (item) => declineGroupInvitation(item.id)
+        }
+      );
+      break;
+  }
+
+  const RenderAccordion = ({ item }) =>
+    datas.map((data, id) => (
+      <ListItem key={id} bottomDivider onPress={() => data.onPress(item)}>
+        <Icon name={data.icon} size={30} color="blue" />
+        <ListItem.Content>
+          <ListItem.Title>{data.title}</ListItem.Title>
+        </ListItem.Content>
+      </ListItem>
+    ));
+
+  const RenderImage = ({ item }) => {
+    const imageSource =
+      item.img.length > 0
+        ? { uri: item.img }
+        : require("../../assets/default-profile.png");
+
+    return (
+      <Avatar
+        rounded
+        source={imageSource}
+        size="medium"
+        containerStyle={styles.groupImage}
+      />
+    );
+  };
+
+  const onRightClick = (index) => {
+    if (expandStatus(index)) {
+      changeExpand(null);
+    } else {
+      changeExpand(index);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {items.map((item, index) => (
+        <ListItem.Accordion
+          key={index}
+          bottomDivider
+          content={
+            <>
+              <RenderImage item={item} />
+              <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+              </ListItem.Content>
+            </>
+          }
+          isExpanded={expandStatus(index)}
+          onPress={() => onRightClick(index)}
+        >
+          {expandStatus(index) && <RenderAccordion item={item} />}
+        </ListItem.Accordion>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: "stretch",
+    margin: 10,
+    padding: 5
+  },
+  groupImage: {
+    marginRight: 10,
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
---
+++ src/screens/Friend/GroupListScreen.js
@@ -0,0 +1,164 @@
+import React, { useEffect, useState } from "react";
+import {
+  Alert,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { Badge } from "react-native-elements";
+import { fetchGroup, checkGroupInvitation } from "../../firebase/FetchGroup";
+import RenderGroupLists, {
+  renderType
+} from "../../components/Friend/RenderGroupLists";
+
+const GroupListScreen = ({ navigation }) => {
+  const [groups, setGroups] = useState([]);
+  const [search, setSearch] = useState("");
+  const [expand, setExpand] = useState(null);
+  const [isInvitationExist, setIsInvitationExist] = useState(false);
+
+  useEffect(() => {
+    return fetchGroup({
+      onSuccess: (data) => {
+        setGroups(data);
+      },
+      onFailure: (error) => {
+        Alert.alert("Error", error.message);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    return checkGroupInvitation({
+      onFound: () => {
+        setIsInvitationExist(true);
+      },
+      onNotFound: () => {
+        setIsInvitationExist(false);
+      },
+      onFailure: (error) => {
+        Alert.alert("Error", error.message);
+      }
+    });
+  }, []);
+
+  return (
+    <ScrollView style={styles.container}>
+      <TextInput
+        value={search}
+        onChangeText={(text) => setSearch(text)}
+        placeholder="Search group by name"
+        style={styles.textInput}
+      />
+
+      <Text style={styles.title}>Groups List</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.replace("FriendList")}
+      >
+        <Text>Back to friend's list</Text>
+      </TouchableOpacity>
+
+      <View style={styles.buttonGroup}>
+        <TouchableOpacity
+          style={styles.groupButton}
+          onPress={() => navigation.navigate("GroupCreation")}
+        >
+          <Text style={styles.createGroupText}>Create Group</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.groupButton}
+          onPress={() => navigation.navigate("GroupRequests")}
+        >
+          <View style={styles.invitationContainer}>
+            <Text style={styles.invitationText}>Group Invitations</Text>
+            {isInvitationExist && (
+              <Badge
+                status="primary"
+                value="!"
+                containerStyle={styles.invitationBadge}
+              />
+            )}
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      <RenderGroupLists
+        type={renderType.GROUP}
+        items={groups.filter((item) =>
+          item.name.toLowerCase().startsWith(search.toLowerCase())
+        )}
+        navigation={navigation}
+        expandStatus={(index) => expand === index}
+        changeExpand={setExpand}
+      />
+    </ScrollView>
+  );
+};
+
+export default GroupListScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "darkcyan",
+    padding: 5,
+    flex: 1
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 5,
+    backgroundColor: "white",
+    color: "black",
+    borderRadius: 10,
+    padding: 2
+  },
+  button: {
+    margin: 5,
+    padding: 5,
+    backgroundColor: "aquamarine",
+    borderRadius: 10
+  },
+  buttonGroup: {
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    flexDirection: "row"
+  },
+  groupButton: {
+    marginHorizontal: 10,
+    padding: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "cyan",
+    flex: 1
+  },
+  invitationContainer: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  createGroupText: {
+    textAlign: "center",
+    fontSize: 12
+  },
+  invitationText: {
+    textAlign: "center",
+    fontSize: 12,
+    flex: 4
+  },
+  invitationBadge: {
+    flex: 1
+  },
+  title: {
+    fontFamily: Platform.OS === "ios" ? "Gill Sans" : "serif",
+    fontSize: 30,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
+  }
+});
