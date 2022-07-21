e13abdabef9cf8fa068c28fbae001c0b5364d63f
elben85 <elbert.benedict@gmail.com>
2022-07-21 22:55:42 +0800

    Add Auth Loading

--- src/screens/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen.js
@@ -11,7 +11,6 @@ import {
   AuthTitle,
   GradientBackground
 } from "../styles/AuthStyles";
-import { Button, ButtonText, RoundedImage } from "../styles/GeneralStyles";
 import { isValidEmail } from "../services/Authentication/CheckCredentials";
 import { sendForgotPasswordEmail } from "../services/Authentication/HandleAuthentication";
 import {
@@ -19,46 +18,52 @@ import {
   redirectToSignupScreen
 } from "../services/Authentication/AuthNavigation";
 import AnimatedInput from "../components/Miscellaneous/AnimatedInput";
+import Loading from "../components/Miscellaneous/Loading";
 
 const ForgotPasswordScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleSubmit() {
+    setIsLoading(true);
     if (!isValidEmail(email)) {
       Alert.alert("Email is not valid");
     } else {
       await sendForgotPasswordEmail(email);
+      setIsLoading(false);
     }
   }
 
   return (
     <GradientBackground>
-      <AuthContainer>
-        <AppLogo source={require("../assets/logo.png")} />
-        <AuthSystemContainer>
-          <AuthTitle> Forgot Password </AuthTitle>
-          <AnimatedInput
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
-          />
-          <AuthPrimaryButton onPress={handleSubmit}>
-            <AuthPrimaryText>Send Password Reset Email</AuthPrimaryText>
-          </AuthPrimaryButton>
-          <AuthSecondaryButton
-            onPress={() => redirectToSignupScreen(navigation)}
-          >
-            <AuthSecondaryText>
-              Don't Have an Account? Sign Up Here
-            </AuthSecondaryText>
-          </AuthSecondaryButton>
-          <AuthSecondaryButton
-            onPress={() => redirectToLoginScreen(navigation)}
-          >
-            <AuthSecondaryText>Have an account? Login Now</AuthSecondaryText>
-          </AuthSecondaryButton>
-        </AuthSystemContainer>
-      </AuthContainer>
+      <Loading isLoading={isLoading}>
+        <AuthContainer>
+          <AppLogo source={require("../assets/logo.png")} />
+          <AuthSystemContainer>
+            <AuthTitle> Forgot Password </AuthTitle>
+            <AnimatedInput
+              placeholder="Email"
+              value={email}
+              onChangeText={setEmail}
+            />
+            <AuthPrimaryButton onPress={handleSubmit}>
+              <AuthPrimaryText>Send Password Reset Email</AuthPrimaryText>
+            </AuthPrimaryButton>
+            <AuthSecondaryButton
+              onPress={() => redirectToSignupScreen(navigation)}
+            >
+              <AuthSecondaryText>
+                Don't Have an Account? Sign Up Here
+              </AuthSecondaryText>
+            </AuthSecondaryButton>
+            <AuthSecondaryButton
+              onPress={() => redirectToLoginScreen(navigation)}
+            >
+              <AuthSecondaryText>Have an account? Login Now</AuthSecondaryText>
+            </AuthSecondaryButton>
+          </AuthSystemContainer>
+        </AuthContainer>
+      </Loading>
     </GradientBackground>
   );
 };
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -11,7 +11,6 @@ import {
   AuthSecondaryButton,
   AuthSecondaryText
 } from "../styles/AuthStyles";
-import { Button, ButtonText, RoundedImage } from "../styles/GeneralStyles";
 import {
   isUsernameTaken,
   isValidUsername
@@ -26,6 +25,7 @@ import {
   redirectToLoginScreen
 } from "../services/Authentication/AuthNavigation";
 import AnimatedInput from "../components/Miscellaneous/AnimatedInput";
+import Loading from "../components/Miscellaneous/Loading";
 
 const initialState = {
   username: "",
@@ -36,6 +36,7 @@ const initialState = {
 
 const SignupScreen = ({ navigation }) => {
   const [credentials, setCredentials] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleChangeText(text, name) {
     setCredentials({
@@ -48,6 +49,7 @@ const SignupScreen = ({ navigation }) => {
 
   async function handleSubmit() {
     console.log("clicked");
+    setIsLoading(true);
     const isUsernameNotAvailable = await isUsernameTaken(credentials.username);
 
     if (!isValidUsername(credentials.username)) {
@@ -55,64 +57,71 @@ const SignupScreen = ({ navigation }) => {
         "invalid username",
         "username must only contains alphanumeric characters and must at least be 1 character long"
       );
+      setIsLoading(false);
     } else if (isUsernameNotAvailable) {
       Alert.alert("username is not available");
+      setIsLoading(false);
     } else if (!isValidEmail(credentials.email)) {
       Alert.alert("email is not valid!");
+      setIsLoading(false);
     } else if (credentials.password !== credentials.confirmPass) {
       Alert.alert("password doesn't match");
+      setIsLoading(false);
     } else if (isPasswordTooShort(credentials.password)) {
       Alert.alert("password must at least be 6 characters long");
+      setIsLoading(false);
     } else {
-      signUp(credentials, () => redirectToLoginScreen(navigation));
+      await signUp(credentials, () => redirectToLoginScreen(navigation));
     }
   }
 
   return (
     <GradientBackground>
-      <AuthContainer>
-        <AppLogo source={require("../assets/logo.png")} />
-        <AuthSystemContainer>
-          <AuthTitle> Sign Up </AuthTitle>
-          <AnimatedInput
-            placeholder="Username"
-            value={credentials.username}
-            onChangeText={(text) => handleChangeText(text, "username")}
-          />
-          <AnimatedInput
-            placeholder="Email"
-            value={credentials.email}
-            onChangeText={(text) => handleChangeText(text, "email")}
-          />
-          <AnimatedInput
-            placeholder="Password"
-            secureTextEntry={true}
-            value={credentials.password}
-            onChangeText={(text) => handleChangeText(text, "password")}
-          />
-          <AnimatedInput
-            placeholder="Confirm Password"
-            secureTextEntry={true}
-            value={credentials.confirmPass}
-            onChangeText={(text) => handleChangeText(text, "confirmPass")}
-          />
-          <AuthPrimaryButton onPress={handleSubmit}>
-            <AuthPrimaryText>Sign Up</AuthPrimaryText>
-          </AuthPrimaryButton>
-          <AuthSecondaryButton
-            onPress={() => redirectToLoginScreen(navigation)}
-          >
-            <AuthSecondaryText>Have an account? Login Now</AuthSecondaryText>
-          </AuthSecondaryButton>
-          <AuthSecondaryButton
-            onPress={() => redirectToForgotPasswordScreen(navigation)}
-          >
-            <AuthSecondaryText>
-              Forgot Your Password? Click Here
-            </AuthSecondaryText>
-          </AuthSecondaryButton>
-        </AuthSystemContainer>
-      </AuthContainer>
+      <Loading isLoading={isLoading}>
+        <AuthContainer>
+          <AppLogo source={require("../assets/logo.png")} />
+          <AuthSystemContainer>
+            <AuthTitle> Sign Up </AuthTitle>
+            <AnimatedInput
+              placeholder="Username"
+              value={credentials.username}
+              onChangeText={(text) => handleChangeText(text, "username")}
+            />
+            <AnimatedInput
+              placeholder="Email"
+              value={credentials.email}
+              onChangeText={(text) => handleChangeText(text, "email")}
+            />
+            <AnimatedInput
+              placeholder="Password"
+              secureTextEntry={true}
+              value={credentials.password}
+              onChangeText={(text) => handleChangeText(text, "password")}
+            />
+            <AnimatedInput
+              placeholder="Confirm Password"
+              secureTextEntry={true}
+              value={credentials.confirmPass}
+              onChangeText={(text) => handleChangeText(text, "confirmPass")}
+            />
+            <AuthPrimaryButton onPress={handleSubmit}>
+              <AuthPrimaryText>Sign Up</AuthPrimaryText>
+            </AuthPrimaryButton>
+            <AuthSecondaryButton
+              onPress={() => redirectToLoginScreen(navigation)}
+            >
+              <AuthSecondaryText>Have an account? Login Now</AuthSecondaryText>
+            </AuthSecondaryButton>
+            <AuthSecondaryButton
+              onPress={() => redirectToForgotPasswordScreen(navigation)}
+            >
+              <AuthSecondaryText>
+                Forgot Your Password? Click Here
+              </AuthSecondaryText>
+            </AuthSecondaryButton>
+          </AuthSystemContainer>
+        </AuthContainer>
+      </Loading>
     </GradientBackground>
   );
 };
