2edf3e9ba5717b3c9faff8acc10a3c0f31f64719
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-07-21 14:52:44 +0700

    Add more gap between icons on Chat Header

--- src/components/Chat/ChatHeader.js
+++ src/components/Chat/ChatHeader.js
@@ -1,4 +1,4 @@
-import { Dimensions, Platform } from "react-native";
+import { Dimensions, Platform, View } from "react-native";
 import React from "react";
 import { chatType } from "../../constants/Chat";
 import styled from "styled-components/native";
@@ -119,9 +119,9 @@ const SectionContainer = styled.View`
 `;
 
 const RowSectionContainer = styled.View`
-  flex: 3;
+  flex: 4;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-evenly;
 `;
 
 const ProfilePicture = styled.Image`
