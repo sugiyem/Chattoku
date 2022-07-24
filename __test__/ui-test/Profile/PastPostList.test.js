35420b0eb3bbafae722891e27098200ca07b0c75
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-07-24 18:22:07 +0700

    Update snapshot

--- __test__/ui-test/Profile/PastPostList.test.js
+++ __test__/ui-test/Profile/PastPostList.test.js
@@ -2,31 +2,10 @@ import React from "react";
 import renderer from "react-test-renderer";
 import PastPostList from "../../../src/components/Profile/PastPostList";
 
-jest.mock("@react-navigation/native", () => ({
-  useNavigation: () => ({
-    navigate: jest.fn()
-  })
-}));
-
 describe("Test Past Post List UI", () => {
   it("Renders correctly", () => {
     const tree = renderer
-      .create(
-        <PastPostList
-          postsData={[
-            {
-              forumData: { img: "forum-img", title: "First Forum" },
-              postData: {
-                timestamp: new Date(2022, 4, 8),
-                lastEdited: new Date(2022, 5, 5),
-                title: "Post Title",
-                content: "Post Content"
-              }
-            }
-          ]}
-          Header={() => <></>}
-        />
-      )
+      .create(<PastPostList postsData={[]} Header={() => <></>} />)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
